5131b5f965ec3e5f9dca82c4bfd32c15c4f714bb
Сергей Шубин <bassdriver83@yandex.ru>
2023-01-09 13:29:37 +0300

    foodgram_v1

---
+++ backend/foodgram/recipes/management/commands/from_csv_to_data.py
@@ -0,0 +1,15 @@
+import csv
+
+from django.core.management.base import BaseCommand
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        with open('recipes/data/ingredients.csv', encoding='UTF-8') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                name, measurement_unit = row
+                Ingredient.objects.get_or_create(
+                    name = name, measurement_unit=measurement_unit
+                )
